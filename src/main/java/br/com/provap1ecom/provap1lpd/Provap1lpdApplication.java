629656b6fd5840862d48e2dc3567c57de4380876
João Marcos <jotamarcosangelo@gmail.com>
2021-09-23 18:14:10 -0300

    feat: Configuração inicial Springboot

---
+++ src/main/java/br/com/provap1ecom/provap1lpd/Provap1lpdApplication.java
@@ -0,0 +1,13 @@
+package br.com.provap1ecom.provap1lpd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Provap1lpdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Provap1lpdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/provap1ecom/provap1lpd/Provap1lpdApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.provap1ecom.provap1lpd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Provap1lpdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
